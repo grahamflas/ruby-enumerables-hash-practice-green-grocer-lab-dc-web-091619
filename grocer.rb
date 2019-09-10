6cb67f51985e8b92a17a57159a0de3ea460196c8
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-10 01:38:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,12 @@
 def consolidate_cart(cart)
-  new_hash = {}
+    new_hash = Hash.new
+
     cart.each do |item|
-        if new_hash[item.keys[0]]
-            new_hash[item.keys[0]][:count] += 1
-        else
         new_hash[item.keys[0]] = {
-            price: item.values[0][:price],
-            clearance: item.values[0][:clearance],
+            price: [item.values[0]][:price],
+            clearance: [item.values[0]][:clearance],
             count: 1
         }
-        end
     end
     new_hash
 end
